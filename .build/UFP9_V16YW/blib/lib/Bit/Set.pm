a2551a514317fef809a6c652e2d95ebd8021d167
chrisarg <chrisarg.gmail.com>
2025-07-09 20:56:44 -0600

    put test file for memory management

---
+++ .build/UFP9_V16YW/blib/lib/Bit/Set.pm
@@ -0,0 +1,192 @@
+package Bit::Set;
+$Bit::Set::VERSION = '0.01';
+use strict;
+use warnings;
+
+use Alien::Bit;
+use FFI::Platypus;
+use FFI::Platypus::Memory qw(malloc free);
+use FFI::Platypus::Buffer qw(scalar_to_buffer buffer_to_scalar);
+use Module::Find;
+useall Bit::Set::DB;
+
+=head1 NAME
+
+Bit::Set - Provides an interface to the Bit libraries for bitset manipulations
+
+=head1 VERSION
+
+version 0.01
+
+=cut
+
+# Create FFI::Platypus object
+my $ffi = FFI::Platypus->new(api => 2);
+
+# Add path to our dynamic library
+$ffi->lib(Alien::Bit->dynamic_libs);
+
+# Define opaque types for our bitset pointers
+$ffi->type('opaque' => 'Bit_T');
+$ffi->type('opaque*' => 'Bit_T_Ptr');
+
+# Define our Bit_T functions
+$ffi->attach(Bit_new => ['int'] => 'Bit_T' => sub {
+    my ($xsub, $self, $length) = @_;
+    die "Length must be a positive integer" unless defined $length && $length > 0;
+    my $bit_set = $xsub->($length);
+    die "Failed to create bit set" unless $bit_set;
+    return bless { _handle => $bit_set }, $self;
+});
+
+$ffi->attach(Bit_free => ['Bit_T_Ptr'] => 'opaque' => sub {
+    my ($xsub, $self) = @_;
+    my $ptr = \$self->{_handle};
+    return $xsub->($ptr);
+});
+
+$ffi->attach(Bit_length => ['Bit_T'] => 'int' => sub {
+    my ($xsub, $self) = @_;
+    return $xsub->($self->{_handle});
+});
+
+$ffi->attach(Bit_count => ['Bit_T'] => 'int' => sub {
+    my ($xsub, $self) = @_;
+    return $xsub->($self->{_handle});
+});
+
+$ffi->attach(Bit_buffer_size => ['int'] => 'int');
+
+$ffi->attach(Bit_bset => ['Bit_T', 'int'] => 'void' => sub {
+    my ($xsub, $self, $index) = @_;
+    die "Index must be non-negative" unless defined $index && $index >= 0;
+    $xsub->($self->{_handle}, $index);
+});
+
+$ffi->attach(Bit_bclear => ['Bit_T', 'int'] => 'void' => sub {
+    my ($xsub, $self, $index) = @_;
+    die "Index must be non-negative" unless defined $index && $index >= 0;
+    $xsub->($self->{_handle}, $index);
+});
+
+$ffi->attach(Bit_get => ['Bit_T', 'int'] => 'int' => sub {
+    my ($xsub, $self, $index) = @_;
+    die "Index must be non-negative" unless defined $index && $index >= 0;
+    return $xsub->($self->{_handle}, $index);
+});
+
+$ffi->attach(Bit_set => ['Bit_T', 'int', 'int'] => 'void' => sub {
+    my ($xsub, $self, $lo, $hi) = @_;
+    die "Low index must be non-negative" unless defined $lo && $lo >= 0;
+    die "High index must be greater than or equal to low index" unless defined $hi && $hi >= $lo;
+    $xsub->($self->{_handle}, $lo, $hi);
+});
+
+$ffi->attach(Bit_clear => ['Bit_T', 'int', 'int'] => 'void' => sub {
+    my ($xsub, $self, $lo, $hi) = @_;
+    die "Low index must be non-negative" unless defined $lo && $lo >= 0;
+    die "High index must be greater than or equal to low index" unless defined $hi && $hi >= $lo;
+    $xsub->($self->{_handle}, $lo, $hi);
+});
+
+# Comparison operations
+$ffi->attach(Bit_eq => ['Bit_T', 'Bit_T'] => 'int' => sub {
+    my ($xsub, $self, $other) = @_;
+    die "Other bitset must be a Bit::Set object" unless ref $other eq ref $self;
+    return $xsub->($self->{_handle}, $other->{_handle});
+});
+
+# Set operations
+$ffi->attach(Bit_union => ['Bit_T', 'Bit_T'] => 'Bit_T' => sub {
+    my ($xsub, $self, $other) = @_;
+    die "Other bitset must be a Bit::Set object" unless ref $other eq ref $self;
+    my $result_handle = $xsub->($self->{_handle}, $other->{_handle});
+    return bless { _handle => $result_handle }, ref $self;
+});
+
+$ffi->attach(Bit_inter => ['Bit_T', 'Bit_T'] => 'Bit_T' => sub {
+    my ($xsub, $self, $other) = @_;
+    die "Other bitset must be a Bit::Set object" unless ref $other eq ref $self;
+    my $result_handle = $xsub->($self->{_handle}, $other->{_handle});
+    return bless { _handle => $result_handle }, ref $self;
+});
+
+# Count operations
+$ffi->attach(Bit_inter_count => ['Bit_T', 'Bit_T'] => 'int' => sub {
+    my ($xsub, $self, $other) = @_;
+    die "Other bitset must be a Bit::Set object" unless ref $other eq ref $self;
+    return $xsub->($self->{_handle}, $other->{_handle});
+});
+
+# Constructor and destructor
+sub new {
+    my ($class, $length) = @_;
+    return $class->Bit_new($length);
+}
+
+sub DESTROY {
+    my ($self) = @_;
+    $self->Bit_free() if defined $self->{_handle};
+}
+
+# Convenient accessor methods
+sub length {
+    my ($self) = @_;
+    return $self->Bit_length();
+}
+
+sub count {
+    my ($self) = @_;
+    return $self->Bit_count();
+}
+
+sub get {
+    my ($self, $index) = @_;
+    return $self->Bit_get($index);
+}
+
+sub set {
+    my ($self, $index) = @_;
+    $self->Bit_bset($index);
+    return $self;
+}
+
+sub clear {
+    my ($self, $index) = @_;
+    $self->Bit_bclear($index);
+    return $self;
+}
+
+sub set_range {
+    my ($self, $lo, $hi) = @_;
+    $self->Bit_set($lo, $hi);
+    return $self;
+}
+
+sub clear_range {
+    my ($self, $lo, $hi) = @_;
+    $self->Bit_clear($lo, $hi);
+    return $self;
+}
+
+sub equals {
+    my ($self, $other) = @_;
+    return $self->Bit_eq($other) ? 1 : 0;
+}
+
+sub union {
+    my ($self, $other) = @_;
+    return $self->Bit_union($other);
+}
+
+sub intersection {
+    my ($self, $other) = @_;
+    return $self->Bit_inter($other);
+}
+
+sub intersection_count {
+    my ($self, $other) = @_;
+    return $self->Bit_inter_count($other);
+}
+
+1;
---
+++ lib/Bit/Set/DB.pm
@@ -0,0 +1,174 @@
+package Bit::Set::DB;
+
+use strict;
+use warnings;
+use FFI::Platypus;
+use FFI::Platypus::Memory qw(malloc free);
+use FFI::Platypus::Buffer qw(scalar_to_buffer buffer_to_scalar);
+use Bit::Set;
+use Alien::Bit;
+
+=head1 NAME
+
+Bit::Set::DB - Provides an interface for bitset manipulations in contiguous memory buffers (containers)
+
+=cut
+
+package SETOP_COUNT_OPTS {
+    
+    use strict;
+    use warnings;
+    use FFI::Platypus::Record;
+    record_layout_1(
+        'int'=> 'num_cpu_threads',
+        'int'=> 'device_id',
+        'bool'=> 'upd_1st_operand',
+        'bool'=> 'upd_2nd_operand',
+        'bool'=> 'release_1st_operand',
+        'bool'=> 'release_2nd_operand',
+        'bool'=> 'release_counts'
+    );
+};
+# Create FFI::Platypus object
+my $ffi = FFI::Platypus->new(api => 2);
+
+# Add path to our dynamic library
+$ffi->lib(Alien::Bit->dynamic_libs);
+
+# Define opaque types for our bitset pointers
+$ffi->type('opaque' => 'Bit_T');
+$ffi->type('opaque' => 'Bit_T_DB');
+$ffi->type('opaque*' => 'Bit_T_DB_Ptr');
+
+# Define struct for SETOP_COUNT_OPTS
+$ffi->type('record(SETOP_COUNT_OPTS)' => 'SETOP_COUNT_OPTS');
+
+# Define Bit_T_DB functions
+$ffi->attach(BitDB_new => ['int', 'int'] => 'Bit_T_DB' => sub {
+    my ($xsub, $self, $length, $num_of_bitsets) = @_;
+    die "Length must be a positive integer" unless defined $length && $length > 0;
+    die "Number of bitsets must be a positive integer" unless defined $num_of_bitsets && $num_of_bitsets > 0;
+    my $db = $xsub->($length, $num_of_bitsets);
+    die "Failed to create bitset DB" unless $db;
+    return bless { _handle => $db }, $self;
+});
+
+$ffi->attach(BitDB_free => ['Bit_T_DB_Ptr'] => 'opaque' => sub {
+    my ($xsub, $self) = @_;
+    my $ptr = \$self->{_handle};
+    return $xsub->($ptr);
+});
+
+$ffi->attach(BitDB_length => ['Bit_T_DB'] => 'int' => sub {
+    my ($xsub, $self) = @_;
+    return $xsub->($self->{_handle});
+});
+
+$ffi->attach(BitDB_nelem => ['Bit_T_DB'] => 'int' => sub {
+    my ($xsub, $self) = @_;
+    return $xsub->($self->{_handle});
+});
+
+$ffi->attach(BitDB_count_at => ['Bit_T_DB', 'int'] => 'int' => sub {
+    my ($xsub, $self, $index) = @_;
+    die "Index must be non-negative" unless defined $index && $index >= 0;
+    return $xsub->($self->{_handle}, $index);
+});
+
+$ffi->attach(BitDB_clear => ['Bit_T_DB'] => 'void' => sub {
+    my ($xsub, $self) = @_;
+    $xsub->($self->{_handle});
+});
+
+$ffi->attach(BitDB_clear_at => ['Bit_T_DB', 'int'] => 'void' => sub {
+    my ($xsub, $self, $index) = @_;
+    die "Index must be non-negative" unless defined $index && $index >= 0;
+    $xsub->($self->{_handle}, $index);
+});
+
+$ffi->attach(BitDB_get_from => ['Bit_T_DB', 'int'] => 'Bit_T' => sub {
+    my ($xsub, $self, $index) = @_;
+    die "Index must be non-negative" unless defined $index && $index >= 0;
+    my $bit_handle = $xsub->($self->{_handle}, $index);
+    return bless { _handle => $bit_handle }, 'Bit::Set';
+});
+
+$ffi->attach(BitDB_put_at => ['Bit_T_DB', 'int', 'Bit_T'] => 'void' => sub {
+    my ($xsub, $self, $index, $bitset) = @_;
+    die "Index must be non-negative" unless defined $index && $index >= 0;
+    die "Bitset must be a Bit::Set object" unless ref $bitset eq 'Bit::Set';
+    $xsub->($self->{_handle}, $index, $bitset->{_handle});
+});
+
+# CPU-specific intersection count function
+$ffi->attach(BitDB_inter_count_cpu => ['Bit_T_DB', 'Bit_T_DB', 'SETOP_COUNT_OPTS'] => 'int*' => sub {
+    my ($xsub, $self, $other, $opts) = @_;
+    die "Other must be a Bit::Set::DB object" unless ref $other eq ref $self;
+    $opts ||= { 
+        num_cpu_threads => 1, 
+        device_id => -1, 
+        upd_1st_operand => 0, 
+        upd_2nd_operand => 0,
+        release_1st_operand => 0,
+        release_2nd_operand => 0,
+        release_counts => 0
+    };
+    return $xsub->($self->{_handle}, $other->{_handle}, $opts);
+});
+
+# Constructor and destructor
+sub new {
+    my ($class, $length, $num_of_bitsets) = @_;
+    return $class->BitDB_new($length, $num_of_bitsets);
+}
+
+sub DESTROY {
+    my ($self) = @_;
+    $self->BitDB_free() if defined $self->{_handle};
+}
+
+# Convenient accessor methods
+sub length {
+    my ($self) = @_;
+    return $self->BitDB_length();
+}
+
+sub num_of_bitsets {
+    my ($self) = @_;
+    return $self->BitDB_nelem();
+}
+
+sub count_at {
+    my ($self, $index) = @_;
+    return $self->BitDB_count_at($index);
+}
+
+sub clear {
+    my ($self) = @_;
+    $self->BitDB_clear();
+    return $self;
+}
+
+sub clear_at {
+    my ($self, $index) = @_;
+    $self->BitDB_clear_at($index);
+    return $self;
+}
+
+sub get_bitset {
+    my ($self, $index) = @_;
+    return $self->BitDB_get_from($index);
+}
+
+sub put_bitset {
+    my ($self, $index, $bitset) = @_;
+    $self->BitDB_put_at($index, $bitset);
+    return $self;
+}
+
+sub intersection_count_cpu {
+    my ($self, $other, $opts) = @_;
+    return $self->BitDB_inter_count_cpu($other, $opts);
+}
+
+1;
